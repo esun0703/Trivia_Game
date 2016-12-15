963ac8cc5bb90d520dded91e92567f8151408fa0
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2016-12-14 21:52:44 -0500

    index.html: added choiceDisplay div style.css: changed background color of questionDisplay, adjusted margins and padding questionDisplay, added #timer, changed heights of jumbotron and container, game.js: added var timer and all properties inside, displayCurrentQuestion function has added to loop, and add choices, need to fix so that questions show up one at a time instead all at the same time, choices need to show up to be the right one.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,100 @@
+//variables
+//================================================================
+
+//all the questions and their answers
 var questions=[{
 	question:"Where Does 'The Lion King' Take Place?",
-	choices:["Africa","North Pole", "Hawaii", "China"],
-	correctAnswer:"Africa",
+	choices:["Africa","Pallet Town", "Neverland", "Atlantis"],
+	correctAnswer:0,
 }, {
 	question:"What Is The Age Of Ariel In The Little Mermaid?",
 	choices:[13,15,16,18],
-	correctAnswer:18,
+	correctAnswer:2,
 }, {
 	question:"Aladdin Is Also Known As...?",
 	choices:["Jaffar","The Genie", "Raja", "The Diamond In The Rough"],
-	correctAnswer:"The Diamond In The Rough",
+	correctAnswer:3,
 }, {
 	question:"What Is The Name Of The Famous Male Mouse In Disney?",
 	choices:["Goofy", "Mickey", "Minnie", "Pluto"],
-	correctAnswer:"Mickey",
+	correctAnswer:1,
 }, {
-	question:"Why Does Sebastian Think Ariel Should 'Under The Sea?'",
+	question:"Why Does Sebastian Think Ariel Should Stay 'Under The Sea'?",
 	choices:["Up on the shore they work all day Out in the sun they slave away", "Down here all the fish is happy. And soft through the waves they roll", "Nobody beat us fry, us and eat us","All Of The Above"],
-	correctAnswer:"All Of The Above",
+	correctAnswer:3,
 }];
 
+//tracks which question selected on
+var currentQuestion=0;
+//tracks the number of correct answers
+var correctAnswers=0;
+//tracks the number of wrong answers
+var wrongAnswers=0;
+//boullion 
+var quizOver=false;
+
+//tracks
+
+//debugging
 for(let i=0; i<questions.length; i++){
 console.log(questions[i]);
 };
+
+var timer= {
+	
+	time:30,
+
+	start: function(){
+		myTimer=setInterval(timer.count,1000)
+	
+	},
+
+	stop: function(){
+		clearInterval(myTimer);
+	},
+	
+	count: function(){
+		timer.time--;
+		$("#timer").html("00:"+ timer.time);
+	},
+};
+
+
+//Functions
+//================================================================
+function displayCurrentQuestion(){
+	var chosenQuestion;
+	var choice;
+	
+	//gets question...!!!!! so I kind of f-ed up this part
+	for(i=0; i<questions.length;i++){
+		chosenQuestion=questions[i].question;
+		$("#questionDisplay").append(chosenQuestion);
+	}
+	//displays question onto questionDisplay div
+	
+
+	for(i=0;i<questions[i].choices.length;i++){
+		choice=questions[i].choices[i];
+		$('<li><input type="radio" value=' + i + ' name="dynradio"/> &nbsp;' + choice + '</li>').appendTo("#choiceDisplay");
+		}
+
+	//debuggin
+	console.log(chosenQuestion);
+	console.log(choice);
+};
+
+
+
+
+//Game Play
+//================================================================
+$(document).ready(function(){
+	//display the first question
+	$("#play").on("click", function(){
+		displayCurrentQuestion();
+		timer.start();
+	});
+
+});
+
