1a8bad00dae5c97317ee6d28cf8bc1d2afaf1f1f
Emily Sun <ESun0703@Emilys-MacBook-Pro.local>
2016-12-14 19:32:15 -0500

    Added div for timer, style.css added background image, jumbotron aligned, and background color, added header image and formatted, and font formatting for h3, game.js added array of objects for questions.

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+var questions=[{
+	question:"Where Does 'The Lion King' Take Place?",
+	choices:["Africa","North Pole", "Hawaii", "China"],
+	correctAnswer:"Africa",
+}, {
+	question:"What Is The Age Of Ariel In The Little Mermaid?",
+	choices:[13,15,16,18],
+	correctAnswer:18,
+}, {
+	question:"Aladdin Is Also Known As...?",
+	choices:["Jaffar","The Genie", "Raja", "The Diamond In The Rough"],
+	correctAnswer:"The Diamond In The Rough",
+}, {
+	question:"What Is The Name Of The Famous Male Mouse In Disney?",
+	choices:["Goofy", "Mickey", "Minnie", "Pluto"],
+	correctAnswer:"Mickey",
+}, {
+	question:"Why Does Sebastian Think Ariel Should 'Under The Sea?'",
+	choices:["Up on the shore they work all day Out in the sun they slave away", "Down here all the fish is happy. And soft through the waves they roll", "Nobody beat us fry, us and eat us","All Of The Above"],
+	correctAnswer:"All Of The Above",
+}];
+
+for(let i=0; i<questions.length; i++){
+console.log(questions[i]);
+};
